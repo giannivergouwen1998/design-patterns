45ef293cffb7945503aa059109ff0c17eaf5217c
Gianni <g.vergouwen@pararius.nl>
2023-01-06 15:59:46 +0100

    fix share facade

--- src/Facade/ShareFacade.php
+++ src/Facade/ShareFacade.php
@@ -4,18 +4,10 @@ namespace App\Facade;
 
 final class ShareFacade
 {
-    protected Twitter $twitter;
-    protected Google $google;
-    protected Reddit $reddit;
+    public function __construct(protected Twitter $twitter, protected Google $google, protected Reddit $reddit)
+    {}
 
-    public function __construct(Twitter $twitter, Google $google, Reddit $reddit)
-    {
-        $this->twitter = $twitter;
-        $this->google = $google;
-        $this->reddit = $reddit;
-    }
-
-    /** @return array<string> */
+    /** @return array<string, string> */
     public function share(string $url, string $title, string $message): array
     {
         return [
