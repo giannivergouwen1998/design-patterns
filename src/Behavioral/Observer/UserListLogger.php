0bd1e0c6cd2013f67615a62d89dbcb523f2c5b28
Gianni <g.vergouwen@pararius.nl>
2023-04-18 16:24:27 +0200

    tmp

---
+++ src/Behavioral/Observer/UserListLogger.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Behavioral\Observer;
+
+use ReturnTypeWillChange;
+use SplSubject;
+
+final class UserListLogger implements \SplObserver
+{
+
+    #[ReturnTypeWillChange] public function update(SplSubject $subject): string
+    {
+        return 'All observers has been updated';
+    }
+}
---
+++ src/Behavioral/Observer/readme.md
@@ -0,0 +1,5 @@
+# Observer
+
+### Waar gebruik je het voor?
+Je gebruikt de observer voor als je een subscription mechanisme wilt toepassen op objecten.
+Zo kun je meerdere objecten op de hoogte brengen van een event die je uitvoert op het object dat geobserveerd wordt.
